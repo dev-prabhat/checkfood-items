42af87db3648db01dc9198fe85fd28ffa2497e85
Prabhat <singhprabhatp.s.224@gamil.com>
2021-11-06 22:22:45 +0530

    getting unique category from the existing api data in an array

--- src/Components/Basic/Resturant.js
+++ src/Components/Basic/Resturant.js
@@ -4,19 +4,46 @@ import MenuCard from './MenuCard'
 
 import "./styles.css"
 
+const uniqueList = [...new Set(Menu.map((item) => (
+    item.category
+)))]
+
+console.log(uniqueList)
+
+
+
 const Resturant = () => {
 
     const [menuDate, setMenuData] = useState(Menu)
-    console.log(menuDate)
+
+
+    const filterItem = (category) => {
+        const updatedList = Menu.filter((item) => (category === 'all' ? Menu : item.category === category
+        ))
+
+        setMenuData(updatedList)
+    }
 
 
     return (
-        <div className="card-container">
-            <section className="main-card--container">
-                {
-                    menuDate.map(item => <MenuCard item={item} key={item.id} />)
-                }
-            </section>
+        <div>
+            <nav className="navbar">
+                <div className='btn-group'>
+                    <button className="btn-group__item" onClick={() => filterItem("breakfast")}>Breakfast</button>
+                    <button className="btn-group__item" onClick={() => filterItem("lunch")}>Lunch</button>
+                    <button className="btn-group__item" onClick={() => filterItem("snack")}>Snack</button>
+                    <button className="btn-group__item" onClick={() => filterItem("dinner")}>Dinner</button>
+                    <button className="btn-group__item" onClick={() => filterItem("all")}>All</button>
+                </div>
+            </nav>
+
+            <div className="card-container">
+                <section className="main-card--container">
+                    {
+                        menuDate.map(item => <MenuCard item={item} key={item.id} />)
+                    }
+                </section>
+            </div>
         </div>
     )
 }
